8aa55e745ef7e36b3f14343c4287526d492045d8
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-04 08:04:50 +0000

    poo

--- 01/intro.cpp
+++ 01/intro.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+class Aviao{
+	
+	public:
+		int vel=0;
+		int velMax;
+		string tipo;
+		void ini(int tp);
+		
+	private:	
+};
+
 int main() {
+	
+	Aviao *av1 = new Aviao();
+	cout << av1->vel;
 	
 	return 0;
 }
